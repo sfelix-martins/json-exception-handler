6bfb1986e64ec5df79252e6952e3f36626493529
Samuel Martins <samuelmartins@negociosreais.com.br>
2017-11-14 21:30:57 -0200

    Fixed validation if clientException is not generated by guzzle client

--- src/ClientHandler.php
+++ src/ClientHandler.php
@@ -7,28 +7,45 @@ use GuzzleHttp\Exception\ClientException;
 
 trait ClientHandler
 {
-    public function clientException(ClientException $exception)
+    public function clientException($exception)
     {
         $statusCode = 500;
         $title = 'client_exception';
-        $code = config($this->configFile.'codes.client.default');
-        $detail = __('exception::exceptions.client.unavailable');
+        $detail = $exception->getMessage();
+        $code = $this->getCode();
 
-        $requestHost = $exception->getRequest()->getUri()->getHost();
-        $clientCausers = $this->clientExceptionCausers();
+        if ($exception instanceof ClientException) {
+            $requestHost = $exception->getRequest()->getUri()->getHost();
+            $clientCausers = $this->clientExceptionCausers();
 
-        if ($clientCausers->isPagarme($requestHost)) {
-            $code = config('json-exception-handler.codes.client.pagarme') ?? 'pagarme';
-        } elseif ($clientCausers->isMailgun($requestHost)) {
-            $code = config('json-exception-handler.codes.client.mailgun') ?? 'mailgun';
-        }
-
-        if (App::environment('production')) {
-            $detail = $detail.' #'.$code;
-        } else {
             $response = $exception->getResponse();
-            $detail = json_decode($response->getBody())->message;
-            $statusCode = $response->getStatusCode();
+
+            if ($clientCausers->isPagarme($requestHost)) {
+                $code = config('json-exception-handler.codes.client.pagarme') ?? 'pagarme';
+                $errors = json_decode($response->getBody())->errors;
+
+                $firstErrorMessage = '';
+                foreach ($errors as $error) {
+                    $firstErrorMessage = $error->message;
+                    break;
+                }
+
+                $detailedError = $firstErrorMessage.' #'.$code;
+            } elseif ($clientCausers->isMailgun($requestHost)) {
+                $code = config('json-exception-handler.codes.client.mailgun') ?? 'mailgun';
+                $detailedError = json_decode($response->getBody())->message.' #'.$code;
+            } else {
+                // Unknown error
+                $code = config('json-exception-handler.codes.client.default');
+            }
+
+            if (App::environment('production')) {
+                $detail = __('exception::exceptions.client.unavailable').' #'.$code;
+            } else {
+                // Return more details about error
+                $detail = $detailedError ?? $detail;
+                $statusCode = $response->getStatusCode();
+            }
         }
 
         $error = [[
